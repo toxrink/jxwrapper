f47bb26ef6862020a6e0cf417c06a723fd5d6d6d
tox <toxrink@hotmail.com>
2020-11-06 16:18:11 +0800

    js工具调整

--- src/main/scala/x/js/JsWrapper.scala
+++ src/main/scala/x/js/JsWrapper.scala
@@ -1,7 +1,9 @@
 package x.js
 
-import x.js.b.Jse
+import javax.script.{Invocable, ScriptEngine, ScriptEngineManager, ScriptException}
 import x.js.i.Function
+
+import scala.io.Source
 
 /**
   * Created by xw on 2019/8/1.
@@ -16,5 +18,22 @@ object JsWrapper {
     * @return
     */
   @throws[Exception]
-  def loadJs[T](jsPath: String, method: String): Function[T] = new Jse[T](jsPath).setFunction(method)
+  def loadJs[T](jsPath: String, method: String): Function[T] = DefaultFunction(jsPath, method)
+
+  case class DefaultFunction[T](jsPath: String, method: String) extends x.js.i.Function[T] {
+    var manager: ScriptEngineManager = new ScriptEngineManager()
+    var engine: ScriptEngine = manager.getEngineByName("nashorn")
+
+    private val reader = Source.fromFile(jsPath, "UTF-8").bufferedReader()
+    engine.eval(reader)
+    reader.close()
+
+    @throws[NoSuchMethodException]
+    @throws[ScriptException]
+    override def call(param: T): T = {
+      val run = engine.asInstanceOf[Invocable]
+      val value = run.invokeFunction(method, param.asInstanceOf[Object])
+      value.asInstanceOf[T]
+    }
+  }
 }
